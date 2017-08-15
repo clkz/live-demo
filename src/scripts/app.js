4433c0c8e44eb53e6c435e82a1c8c25b602d445f
clark <zhouchanghong@dcpai.cn>
2017-08-15 11:13:18 +0800

    add angular env

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,10 @@
-console.log(123131321);
+var app = angular.module('members', []);
+
+app.controller('mainCtrl', ['$scope', function ($scope) {
+
+
+    $scope.model = {
+        members: [{ name: '会员1' }, { name: '会员2' }]
+    }
+
+}])
