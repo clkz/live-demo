3f2dca591002880203e90a58cc622b260d7a62e8
clark <zhouchanghong@dcpai.cn>
2017-08-15 12:16:18 +0800

    add buton

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,8 +1,13 @@
 var app = angular.module('members', []);
 
 app.controller('mainCtrl', ['$scope', function ($scope) {
+
     $scope.model = {
+        date: new Date(),
         members: [{ name: '会员1' }, { name: '会员2' }]
+    }
+    $scope.formModel = {
+        currentDate: new Date()
     }
 
 }])
