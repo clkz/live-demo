a70b1c96d9fd1007578ded06cd64f5b0ff9d96e3
clark <zhouchanghong@dcpai.cn>
2017-08-15 11:19:04 +0800

    add ng-cloak

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,8 +1,6 @@
 var app = angular.module('members', []);
 
 app.controller('mainCtrl', ['$scope', function ($scope) {
-
-
     $scope.model = {
         members: [{ name: '会员1' }, { name: '会员2' }]
     }
